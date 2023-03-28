baf7e44735a38895fa4e1f0630865b411f33a172
Calvin Chern <calvin.chern24@gmail.com>
2023-03-27 22:57:23 -0400

    Add resume

---
+++ src/components/Resume.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <div className="columns">
+      <div className="column">
+        <p className="content is-large">Resume</p>
+        <hr />
+
+        <a
+          className="button is-dark"
+          href="/calvinchern.pdf"
+          target="_blank" rel="noreferrer"
+        >
+          <span className="icon">
+            <i className="fas fa-download"></i>
+          </span>
+          <span>Download My Resume</span>
+        </a>
+      </div>
+      <div className="column">
+        <p className="content is-medium">Skills</p>
+        <hr />
+        <ul>
+            <li>HTML, CSS, Javascript</li>
+            <li>React, JSON, Git</li>
+            <li>Express, NoSQL, MySQL</li>
+            <li>Node.js, jQuery, npm</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
